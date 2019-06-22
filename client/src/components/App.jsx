a509703ff7682202ebf78c5ab0912d90f3ad462c
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-06-22 15:45:03 -0700

    Create input field for pokemon queries with working onChange handler. Add non-functioning button to pull queried pokemon. Includes basic color and text styling client-side.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,12 +4,32 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: ''
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
   }
 
   render() {
     return (
-      <div>Hello World</div>
-    )
+      <div>
+        
+          <div className="pokemonName">Pokémon Name</div>
+          <input type="text" name="name" value={this.state.value}
+            onChange={ (e) => {this.handleNameChange(e)} }>
+          </input>
+          <br></br>
+          <button type="button" className="getPokemon" onClick={this.handleNameChangeClick}>
+            GET Pokémon
+          </button>
+
+      </div>
+    );
   }
 }
 
